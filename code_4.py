80b79256675fe35d376d0191c4db9c7a695c9575
OlmosJavier <jolmos3@miners.utep.edu>
2023-09-29 21:28:02 -0800

    CODE4OlmosR

--- code_4.py
+++ code_4.py
@@ -1,18 +1,43 @@
-import csv
 import pandas as pd
+
 big_mac_file = './big-mac-full-index.csv'
+def get_big_mac_price_by_year(file_path, year, country_code):
+    total_price = 0
+    count = 0
 
-def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    with open(file_path, 'r') as file:
+        reader = pd.DictReader(file)
+        for row in reader:
+            if row['year'] == year and row['country_code'] == country_code:
+                total_price += float(row['dollar_price'])
+                count += 1
 
-def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    return round(total_price / count, 2) if count > 0 else None
+price = get_big_mac_price_by_year('big_mac_data.csv', '2023', 'us')
+print(price)
 
+      
+
+def get_big_mac_price_by_country(country, big_mac_file):
+    df = pd.read_csv(big_mac_file)
+    return round(df[df['country'] == country]['dollar_price'].mean(), 2)
+
+     
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+   def get_the_cheapest_big_mac_price_by_year(year, big_mac_file):
+    df = pd.read_csv(big_mac_file)
+    min_price_row = df[df['year'] == year]['dollar_price'].idxmin()
+    return f"{df.loc[min_price_row, 'country']}({df.loc[min_price_row, 'country_code']}): ${df.loc[min_price_row, 'dollar_price']}"
+    
 
-def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
 
+def get_the_most_expensive_big_mac_price_by_year(year, big_mac_file):
+    df = pd.read_csv(big_mac_file)
+    max_price_row = df[df['year'] == year]['dollar_price'].idxmax()
+    return f"{df.loc[max_price_row, 'country']}({df.loc[max_price_row, 'country_code']}): ${df.loc[max_price_row, 'dollar_price']}"
+    
+
+'''
 if __name__ == "__main__":
     pass # Remove this line and code your user interface
+'''
